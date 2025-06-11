2d1b3f5db97da8159bc2a3469e979046c6f3c622
Shashtttt <160567221+Shashtttt@users.noreply.github.com>
2025-06-11 10:01:20 +0530

    Add files via upload

---
+++ static/questions.js
@@ -0,0 +1,135 @@
+const questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which property is used to change the text color in CSS?",
+    answer: "color",
+    options: [
+      "font-color",
+      "text-color",
+      "color",
+      "background-color"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Which company developed JavaScript?",
+    answer: "Netscape",
+    options: [
+      "Mozilla",
+      "Microsoft",
+      "Netscape",
+      "Oracle"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which HTML attribute is used to define inline styles?",
+    answer: "style",
+    options: [
+      "styles",
+      "style",
+      "class",
+      "font"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which symbol is used for single line comments in JavaScript?",
+    answer: "//",
+    options: [
+      "#",
+      "//",
+      "{}",
+      "/* */"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which method is used to write text in an alert box in JavaScript?",
+    answer: "alert()",
+    options: [
+      "msg()",
+      "prompt()",
+      "alert()",
+      "message()"
+    ]
+  },
+  {
+    numb: 11,
+    question: "How do you call a function named 'myFunction' in JavaScript?",
+    answer: "myFunction()",
+    options: [
+      "call myFunction()",
+      "myFunction()",
+      "call function myFunction()",
+      "call.myFunction()"
+    ]
+  },
+  {
+    numb: 12,
+    question: "What is the correct CSS syntax to make all <p> elements bold?",
+    answer: "p { font-weight: bold; }",
+    options: [
+      "p {text-size: bold;}",
+      "p {font-weight: bold;}",
+      "p {style= bold;}",
+      "p {font: bold;}"
+    ]
+  },
+];
